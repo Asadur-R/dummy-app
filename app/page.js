0d3befd91ea28cdd0b24f5c84d07fd2092731841
Asadur-R <asad.rehman@aurorasolutions.io>
2025-11-06 11:58:43 +0500

    Fixed build

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect, useCallback } from "react";
-import PostForm from "@/components/PostForm";
+import PostForm from "../components/PostForm";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
